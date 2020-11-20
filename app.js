794b44156312b99e8a788b4a45cf3bd44df90f20
Zamo <samuel.gallik@gmail.com>
2020-11-20 18:44:24 +0100

    prototyp 0.0.1 vytvoreny

--- app.js
+++ app.js
@@ -12,8 +12,51 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/skuska", {useNewUrlParser: true, useUnifiedTopology: true} );
+mongoose.pluralize(null);
+
+const formularik = {
+  name: String,
+  content: String
+};
+
+const Formular = mongoose.model("formular", formularik);
+
 app.get("/", (req, res)=>{
-  res.render("home");
+  Formular.find({}, function(err, results){
+  const vysledok = results;
+  res.render("home", {menoAtext: vysledok});
+  });
+
+
+});
+
+app.post("/", (req, res)=>{
+  const name = req.body.name;
+  const text = req.body.textInput;
+
+  const formular =  new Formular({
+    name: name,
+    content: text
+  });
+
+  formular.save();
+  res.redirect("/");
+});
+
+app.post("/delete", (req,res)=>{
+
+
+  const itemToRemove = req.body.itemToRemove;
+
+  Formular.deleteOne({_id: itemToRemove}, function(err){
+    if(err){
+      console.log(err);
+    }else{
+      res.redirect("/");
+    }
+  });
+
 });
 
 app.listen(3000, function(){
